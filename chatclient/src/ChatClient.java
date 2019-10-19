d353d9c4314d89a43125bb328c00ab04f750a472
Duc Anh <ngducanh1223@gmail.com>
2019-10-19 14:45:55 +0700

    Publish client ip in ChatClient

--- chatclient/src/ChatClient.java
+++ chatclient/src/ChatClient.java
@@ -1,13 +1,51 @@
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class ChatClient {
     @SuppressWarnings("InfiniteLoopStatement")
     public static void main(String[] args) {
+        String ip;
+        String hostName = "127.0.0.1";
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (((Enumeration) interfaces).hasMoreElements()) {
+                NetworkInterface iface = interfaces.nextElement();
+                // filters out 127.0.0.1 and inactive interfaces
+                if (iface.isLoopback() || !iface.isUp())
+                    continue;
+
+                Enumeration<InetAddress> addresses = iface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress addr = addresses.nextElement();
+                    if (addr instanceof Inet6Address) continue;
+                    ip = addr.getHostAddress();
+                    if (ip != null && !ip.isEmpty()) {
+                        if (iface.getDisplayName().contains("en")) {
+//                            System.out.println(iface.getDisplayName() + "ethernet" + " " + ip);
+                            hostName = ip;
+                            break;
+                        } else if (iface.getDisplayName().contains("wl")) {
+//                            System.out.println(iface.getDisplayName() + "wifi" + " " + ip);
+                            hostName = ip;
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
+
         if (System.getSecurityManager() == null) {
             System.setProperty("java.security.policy", "client.policy");
             System.setSecurityManager(new SecurityManager());
+            System.out.println(hostName);
+            System.setProperty("java.rmi.server.hostname", hostName);
         }
         try {
             Scanner s = new Scanner(System.in);
@@ -26,7 +64,7 @@ public class ChatClient {
                 msg = s.nextLine().trim();
                 msg = "[" + client.getName() + "] " + msg;
                 server.send(msg);
-                server.getClientById("anon1").send(msg);
+//                server.getClientById("anon1").send(msg);
             }
 
         } catch (Exception e) {
