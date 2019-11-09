a90c25247f0a038fa5153668e032d96a5f48ae03
Duc Anh <ngducanh1223@gmail.com>
2019-11-09 12:09:08 +0700

    Connect server to database

--- chatserver/src/ChatServer.java
+++ chatserver/src/ChatServer.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,11 +12,27 @@ import java.rmi.server.UnicastRemoteObject;
 public class ChatServer {
 
     @SuppressWarnings("InfiniteLoopStatement")
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         if (System.getSecurityManager() == null) {
-//            System.setProperty("java.security.policy","security.policy");
+            System.setProperty("java.security.policy","server.policy");
             System.setSecurityManager(new SecurityManager());
         }
+
+        //CONNECT TO DATABASE
+        String DB_URL = "jdbc:mysql://172.17.0.1:3306/chatapp";
+        String USER_NAME = "root";
+        String PASSWORD = "1";
+        Connection conn = null;
+        conn = getConnection(DB_URL, USER_NAME, PASSWORD);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from users");
+        // show data
+        while (rs.next()) {
+            System.out.println(rs.getInt(1) + "  " + rs.getString(2)
+                    + "  " + rs.getString(3));
+        }
+        // close connection
+        conn.close();
         try {
             String name = "RMIchatapp";
             int port = 6000;
@@ -32,7 +49,6 @@ public class ChatServer {
                     HashMap<String, ChatInterface> clients = server.getClients();
                     for (Map.Entry<String, ChatInterface> clientMap : clients.entrySet()) {
                         ChatInterface client = clientMap.getValue();
-//                        msg = "[" + server.getName() + "] " + msg;
                         client.printMsg("[" + server.getName() + "] " + msg);
                         client.setMsg(msg);
                         client.setIsNewMessage(true);
@@ -43,5 +59,19 @@ public class ChatServer {
             e.printStackTrace();
         }
 
+    }
+
+    private static Connection getConnection(String dbURL, String userName,
+                                            String password) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(dbURL, userName, password);
+            System.out.println("connect successfully!");
+        } catch (Exception ex) {
+            System.out.println("connect failure!");
+            ex.printStackTrace();
+        }
+        return conn;
     }
 }
